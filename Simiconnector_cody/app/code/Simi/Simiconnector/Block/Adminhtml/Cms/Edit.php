6a486fc4a980cee579d9b8f253a2f84bbb2455c8
Cody Nguyen <huyhai.nguyen@trueplus.vn>
2016-08-04 08:48:08 +0700

    cody

---
+++ Simiconnector_cody/app/code/Simi/Simiconnector/Block/Adminhtml/Cms/Edit.php
@@ -0,0 +1,126 @@
+<?php
+namespace Simi\Simiconnector\Block\Adminhtml\Cms;
+
+/**
+ * Admin Connector page
+ *
+ */
+class Edit extends \Magento\Backend\Block\Widget\Form\Container
+{
+    /**
+     * Core registry
+     *
+     * @var \Magento\Framework\Registry
+     */
+    protected $_coreRegistry = null;
+
+    /**
+     * @param \Magento\Backend\Block\Widget\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Widget\Context $context,
+        \Magento\Framework\Registry $registry,
+        array $data = []
+    )
+    {
+        $this->_coreRegistry = $registry;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Initialize cms page edit block
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+
+        $this->_objectId = 'cms_id';
+        $this->_blockGroup = 'Simi_Simiconnector';
+        $this->_controller = 'adminhtml_cms';
+
+        parent::_construct();
+
+        if ($this->_isAllowedAction('Simi_Simiconnector::save')) {
+            $this->buttonList->update('save', 'label', __('Save'));
+            $this->buttonList->add(
+                'saveandcontinue',
+                [
+                    'label' => __('Save and Continue Edit'),
+                    'class' => 'save',
+                    'data_attribute' => [
+                        'mage-init' => [
+                            'button' => ['event' => 'saveAndContinueEdit', 'target' => '#edit_form'],
+                        ],
+                    ]
+                ],
+                -100
+            );
+        } else {
+            $this->buttonList->remove('save');
+        }
+
+        if ($this->_isAllowedAction('Simi_Simiconnector::connector_delete')) {
+            $this->buttonList->update('delete', 'label', __('Delete'));
+        } else {
+            $this->buttonList->remove('delete');
+        }
+    }
+
+    /**
+     * Retrieve text for header element depending on loaded page
+     *
+     * @return string
+     */
+    public function getHeaderText()
+    {
+        if ($this->_coreRegistry->registry('cms')->getId()) {
+            return __("Edit Cms '%1'", $this->escapeHtml($this->_coreRegistry->registry('cms')->getId()));
+        } else {
+            return __('New Cms');
+        }
+    }
+
+    /**
+     * Check permission for passed action
+     *
+     * @param string $resourceId
+     * @return bool
+     */
+    protected function _isAllowedAction($resourceId)
+    {
+        return $this->_authorization->isAllowed($resourceId);
+    }
+
+    /**
+     * Getter of url for "Save and Continue" button
+     * tab_id will be replaced by desired by JS later
+     *
+     * @return string
+     */
+    protected function _getSaveAndContinueUrl()
+    {
+        return $this->getUrl('connector/*/save', ['_current' => true, 'back' => 'edit', 'active_tab' => '{{tab_id}}']);
+    }
+
+    /**
+     * Prepare layout
+     *
+     * @return \Magento\Framework\View\Element\AbstractBlock
+     */
+    protected function _prepareLayout()
+    {
+        $this->_formScripts[] = "
+            function toggleEditor() {
+                if (tinyMCE.getInstanceById('page_content') == null) {
+                    tinyMCE.execCommand('mceAddControl', false, 'page_content');
+                } else {
+                    tinyMCE.execCommand('mceRemoveControl', false, 'page_content');
+                }
+            };
+        ";
+        return parent::_prepareLayout();
+    }
+}
---
+++ Simiconnector_cody/app/code/Simi/Simiconnector/Model/ResourceModel/Cms.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Simi\Simiconnector\Model\ResourceModel;
+
+/**
+ * Connector Resource Model
+ */
+class Cms extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
+{
+    /**
+     * Initialize resource model
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('simiconnector_cms', 'cms_id');
+    }
+}
