01cf26dc8e3dc30994bf05f5f1e8e1f4c6ae7400
Cody Nguyen <cody@simicart.com>
2018-08-14 16:45:47 +0700

    Update url dict

--- app/code/Simi/Simiconnector/Model/Api/Urldicts.php
+++ app/code/Simi/Simiconnector/Model/Api/Urldicts.php
@@ -39,21 +39,27 @@ class Urldicts extends Apiabstract
             $apiModel->setBuilderQuery();
             $result['urldict']['simi_product_data'] = $apiModel->show();
         } else if(isset($result['urldict']['category_id']) && $result['urldict']['category_id']) {
-            $apiModel = $this->simiObjectManager->get('Simi\Simiconnector\Model\Api\Categories');
-            $result['urldict']['simi_catetory_name'] = $this
-                ->simiObjectManager->get('Magento\Catalog\Model\Category')
-                ->load($result['urldict']['category_id'])
-                ->getName();
-            $data['resourceid'] = $result['urldict']['category_id'];
-            $apiModel->pluralKey = 'categories';
-            $apiModel->singularKey = 'category';
-            $apiModel->setData($data);
-            $apiModel->setBuilderQuery();
-            $result['urldict']['simi_category_child'] = $apiModel->show();
+            if (isset($data['params']['get_child_cat']) && $data['params']['get_child_cat']) {
+                $apiModel = $this->simiObjectManager->get('Simi\Simiconnector\Model\Api\Categories');
+                $result['urldict']['simi_catetory_name'] = $this
+                    ->simiObjectManager->get('Magento\Catalog\Model\Category')
+                    ->load($result['urldict']['category_id'])
+                    ->getName();
+                $data['resourceid'] = $result['urldict']['category_id'];
+                $apiModel->pluralKey = 'categories';
+                $apiModel->singularKey = 'category';
+                $apiModel->setData($data);
+                $apiModel->setBuilderQuery();
+                $result['urldict']['simi_category_child'] = $apiModel->show();
+            }
             $productListModel = $this->simiObjectManager
                 ->get('Simi\Simiconnector\Model\Api\Products');
             $data['resourceid'] = null;
             $data['params'][self::FILTER] = array('cat_id'=>$result['urldict']['category_id']);
+            $data['params']['image_width'] = isset($data['params']['image_width'])?
+                $data['params']['image_width']:200;
+            $data['params']['image_height'] = isset($data['params']['image_height'])?
+                $data['params']['image_height']:200;
             $data['params']['limit'] = 12;
             $productListModel->pluralKey = 'products';
             $productListModel->singularKey = 'product';
