351616972ab7d1afc227231a01387d68a3d451d0
Cody Nguyen <cody@simicart.com>
2018-12-26 14:48:17 +0700

    Update profile fixing

--- app/code/Simi/Simiconnector/Model/Customer.php
+++ app/code/Simi/Simiconnector/Model/Customer.php
@@ -119,9 +119,6 @@ class Customer extends \Magento\Framework\Model\AbstractModel
     {
         $data     = $data['contents'];
         $result   = [];
-        $currPass = $data->old_password;
-        $newPass  = $data->new_password;
-        $confPass = $data->com_password;
 
         $customer = $this->simiObjectManager->create('Magento\Customer\Model\Customer');
         $customer->setWebsiteId($this->storeManager->getStore()->getWebsiteId());
@@ -133,9 +130,10 @@ class Customer extends \Magento\Framework\Model\AbstractModel
             'email'     => $data->email,
         ];
 
-        if ($data->change_password == 1) {
+        if (isset($data->change_password) && $data->change_password == 1) {
+            $newPass  = $data->new_password;
+            $confPass = $data->com_password;
             $customer->setChangePassword(1);
-            $oldPass = $this->_getSession()->getCustomer()->getPasswordHash();
             if ($newPass != $confPass) {
                 throw new \Magento\Framework\Exception\InputException(
                     __('Password confirmation doesn\'t match entered password.')
