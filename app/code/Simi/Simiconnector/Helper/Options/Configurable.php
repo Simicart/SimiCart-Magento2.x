4f9355dcb0721cc1156f6b11463ab64e8eb75a40
Cody Nguyen <cody@simicart.com>
2019-02-27 10:10:53 +0700

    Fix configurable option with no product

--- app/code/Simi/Simiconnector/Helper/Options/Configurable.php
+++ app/code/Simi/Simiconnector/Helper/Options/Configurable.php
@@ -28,10 +28,29 @@ class Configurable extends \Simi\Simiconnector\Helper\Options
     {
         $layout = $this->simiObjectManager->get('Magento\Framework\View\LayoutInterface');
         $block  = $layout->createBlock('Magento\ConfigurableProduct\Block\Product\View\Type\Configurable');
-
         $block->setProduct($product);
         $options                         = [];
-        $configurable_options            = json_decode($block->getJsonConfig());
+        $configurable_options            = json_decode($block->getJsonConfig(), 1);
+
+        if (isset($configurable_options['attributes'])) {
+            foreach ($configurable_options['attributes'] as $attribute_code => $attribute_details) {
+                if (isset($attribute_details['options'])) {
+                    $updatedOptions = array();
+                    foreach ($attribute_details['options'] as $option_key => $option_data) {
+                        if (
+                            isset($option_data['products']) &&
+                            is_array($option_data['products']) &&
+                            count($option_data['products']) != 0
+                        ) {
+                            $updatedOptions[] = $option_data;
+                        }
+                    }
+                    $attribute_details['options'] = $updatedOptions;
+                    $configurable_options['attributes'][$attribute_code] = $attribute_details;
+                }
+            }
+        }
+
         $options['configurable_options'] = $configurable_options;
 
         if (!($product->getOptions() === null) && $this->simiObjectManager
