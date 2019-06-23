b72af0057bf0dba5399373b83dbb831bc4a08513
Cody Nguyen <cody@simicart.com>
2019-06-23 15:14:00 +0700

    Update continue customer sesion on connector

--- app/code/Simi/Simiconnector/Api/Simiconnectorapi.php
+++ app/code/Simi/Simiconnector/Api/Simiconnectorapi.php
@@ -1,26 +1,51 @@
 <?php
 namespace Simi\Simiconnector\Api;
-
+use Magento\Authorization\Model\UserContextInterface;
 
 class Simiconnectorapi implements \Simi\Simiconnector\Api\SimiconnectorapiInterface
 {
     protected $request;
     protected $eventManager;
     public $simiObjectManager;
+    private $authorization;
 
     public function __construct(
         \Magento\Framework\ObjectManagerInterface $simiObjectManager,
-        \Magento\Framework\Event\ManagerInterface $eventManager
+        \Magento\Framework\Event\ManagerInterface $eventManager,
+        \Magento\Webapi\Model\Authorization\TokenUserContext $authorization
     )
     {
         $this->simiObjectManager = $simiObjectManager;
         $this->eventManager = $eventManager;
+        $this->authorization = $authorization;
         return $this;
     }
 
+    private function authorization()
+    {
+        try {
+            $userId = $this->authorization->getUserId();
+            $userType = $this->authorization->getUserType();
+            if ($userId && $userType == UserContextInterface::USER_TYPE_CUSTOMER) {
+                $storeManager = $this->simiObjectManager
+                    ->get('\Magento\Store\Model\StoreManagerInterface');
+                $customer = $this->simiObjectManager->get('Magento\Customer\Model\Customer')
+                    ->setWebsiteId($storeManager->getStore()->getWebsiteId())
+                    ->load($userId);
+                if ($customer && $customer->getId()) {
+                    $this->simiObjectManager
+                        ->get('Magento\Customer\Model\Session')
+                        ->setCustomerAsLoggedIn($customer);
+                }
+            }
+        } catch (\Exception $e) {
+
+        }
+    }
 
     private function _getServer()
     {
+        $this->authorization();
         $context = $this->simiObjectManager->create('Simi\Simiconnector\Controller\Rest\V2');
         $serverModel               = $this->simiObjectManager->get('Simi\Simiconnector\Model\Server');
         $serverModel->eventManager = $this->eventManager;
